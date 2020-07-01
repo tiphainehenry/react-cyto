fee2740cafc7c32dcbf36c50e68f9ae818484deb
HENRY Tiphaine TGI/OLS <VTLR2002@orange.com>
2020-07-01 19:40:54 +0200

    working on graph update

--- api/src/utils/graphManager.py
+++ api/src/utils/graphManager.py
@@ -1,5 +1,6 @@
 import os
 import json
+import glob
 
 def initializeGraph(filename):
     with open(filename) as json_data:
@@ -12,12 +13,8 @@ def initializeGraph(filename):
     with open(dataFilename) as json_data:
         dataProj = json.load(json_data)
 
-    # pending = data['markings']['pending']
-    # executed = data['markings']['executed']
-
     for activity_id in included:
         print(activity_id)
-
 
     updProj = []
     id = 0
@@ -31,47 +28,150 @@ def initializeGraph(filename):
         updProj.append(elem)
         id = id + 1
 
-
     with open(os.path.join(dataFilename), 'w') as outfile:
         json.dump(updProj, outfile, indent=2)
 
 
+def retrieveActivityRelations(relations, activity_id, dataProj):
+    conditions = relations['condition']
+    milestones = relations['milestone']
+    responses = relations['response']
+    excludes = relations['exclude']
+    includes = relations['include']
+
+    fromCondition = []
+    cnt = 0
+    for conditionFrom in conditions:
+        if conditionFrom[activity_id] == 1:
+            print(conditionFrom[activity_id])
+            fromCondition.append(dataProj[cnt]['data']['id'])
+        cnt = cnt + 1
+
+    fromMilestone = []
+    cnt = 0
+    for milestoneFrom in milestones:
+        if milestoneFrom[activity_id] == 1:
+            print(milestoneFrom[activity_id])
+            fromMilestone.append(dataProj[cnt]['data']['id'])
+        cnt = cnt + 1
+    
+    toInclude = []
+    cnt = 0
+    for to_include in includes[activity_id]:
+        if to_include == 1:
+            toInclude.append(dataProj[cnt]['data']['id'])
+        cnt = cnt + 1
+
+    toExclude = []
+    cnt = 0
+    for to_exclude in excludes[activity_id]:
+        if to_exclude == 1:
+            toExclude.append(dataProj[cnt]['data']['id'])
+        cnt = cnt + 1
+
+    toRespond = []
+    cnt = 0
+    for to_resp in responses[activity_id]:
+        if to_resp == 1:
+            toRespond.append(dataProj[cnt]['data']['id'])
+        cnt = cnt + 1
+
+    print('Conditions:')
+    print(fromCondition)
+    print('Milestones:')
+    print(fromMilestone)
+    print('Include:')
+    print(toInclude)
+    print('Exclude:')
+    print(toExclude)
+    print('Responses:')
+    print(toRespond)
+
+    return fromCondition, fromMilestone, toInclude, toExclude, toRespond
 
 
-def executeNode(activity_id):
+#def executeNode(data):
+    # projId = data['projId']
+    # activity_name = data['idClicked']
+
+def executeNode(projId, activity_name):
+    status = 'waiting'
+
     # retrieve activity data
+    pData = glob.glob('../../../src/resources/data'+projId+'*')[0]
+    pVect = glob.glob('../../../src/resources/vect'+projId+'*')[0]
 
-    # retrieve activity relations (conditions, milestones, included, excluded, response)
-    # pre_execution evaluation
-        # included? if activity not included (look into classes or markings, equivalent): throw
+    with open(pData) as json_data:
+        dataProj = json.load(json_data)
+    with open(pVect) as json_data:
+        dataVect = json.load(json_data)
 
-        # condition?
-        # for (var i = 0; i < a.condition.length; i++) 
-            # if(!activities[a.condition[i]].executed && activities[a.condition[i]].included) {
-                # throw;
-                # return;
-            # }
-        # milestone?
-        # for (i = 0; i < a.milestone.length; i++) 
-            # if(activities[a.milestone[i]].pending && activities[a.milestone[i]].included) {
-                # throw;
-                # return;
-            # }
+    activity_id = 0
+    while (dataProj[activity_id]['data']['id']!=activity_name):
+        activity_id = activity_id+1
 
-    # upd marking
-        # a.executed = true;
-        # a.pending = false;
+    if ('classes' not in dataProj[activity_id]) or ('included' not in dataProj[activity_id]['classes']):
+        print('[INFO] error - elem not included')
+        return 'throw error - not included'
+
+    else:
+        print('[INFO] success - elem included')
+        status = 'executed'
+
+        # retrieve activity relations (conditions, milestones, included, excluded, response)
+        markings= dataVect['markings'][0]
+        included = markings['included'] 
+        executed = markings['executed']  
+        pending = markings['pending']  
+
+        # retrieve activity relations (conditions, milestones, included, excluded, response)
+        relations = dataVect['relations'][0]
+        fromCondition, fromMilestone, toInclude, toExclude, toRespond = retrieveActivityRelations(relations, 
+        activity_id, dataProj)
+
+        # pre_execution evaluation
+        ## if conditions not empty: get markings of conditions --> if not executed yet and included: error
+
+        ## if milestones not empty: get markings --> similar behavior
 
 
-    # post_execution evaluation  --> upd markings included, excluded, response
 
-        # for (i = 0; i < a.include.length; i++) 
-            # activities[a.include[i]].included = true;
-        # for (i = 0; i < a.exclude.length; i++) 
-            # activities[a.exclude[i]].included = false;
-        # for (i = 0; i < a.response.length; i++) 
-            # activities[a.response[i]].pending = true; 
+            # condition?
+            # for (var i = 0; i < a.condition.length; i++) 
+                # if(!activities[a.condition[i]].executed && activities[a.condition[i]].included) {
+                    # throw;
+                    # return;
+                # }
+            # milestone?
+            # for (i = 0; i < a.milestone.length; i++) 
+                # if(activities[a.milestone[i]].pending && activities[a.milestone[i]].included) {
+                    # throw;
+                    # return;
+                # }
 
-    # rewrite dataRole.json, rewrite , send status
+        # Update markings:
+        executed[activity_id] = 1
+        pending[activity_id] = 0
 
-    print('hello there '+activity_id)
+        # post_execution evaluation  --> upd markings included, excluded, response
+            # for (i = 0; i < a.include.length; i++) 
+                # activities[a.include[i]].included = true;
+            # for (i = 0; i < a.exclude.length; i++) 
+                # activities[a.exclude[i]].included = false;
+            # for (i = 0; i < a.response.length; i++) 
+                # activities[a.response[i]].pending = true; 
+
+        # rewrite dataRole.json, rewrite , send status
+
+        return status
+
+
+
+def main():
+    projId = 'Florist'
+    activity_name = 'CallShipper'
+
+    executeNode(projId, activity_name)
+
+if __name__ == "__main__":
+    main()
