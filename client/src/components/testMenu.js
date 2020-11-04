34c98838ffd49db5940b1a4d3b8fd599cee697a8
Tiphaine Henry <vtlr2002@p-f8-b4-6a-f0-67-59.rd.francetelecom.fr>
2020-11-04 11:12:05 +0100

    test proxy

--- client/src/components/testMenu.js
+++ client/src/components/testMenu.js
@@ -72,7 +72,7 @@ class TestMenu extends React.Component {
 
 
   fileUpload(file){
-    const url = `https://dcr-choreo.herokuapp.com/api/inputFile`;
+    const url = `https://dcrchoreo.herokuapp.com/inputFile`;
     const formData = new FormData();
     formData.append('file',file);
     const config = {
